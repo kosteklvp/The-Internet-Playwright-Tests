fc5ae54eb9ab43bbe7d8f022d6b76590f754e7fd
Piotr Kostański <p.kostanski@dtpoland.com>
2023-05-16 09:11:01 +0200

    Added remaining test steps and skipped tests.

--- tests/AddRemoveElements.spec.ts
+++ tests/AddRemoveElements.spec.ts
@@ -9,7 +9,7 @@ const CLICK_COUNT = [1, 2, 3, 5, 7, 11, 13, 17, 19, 20, 25, 50, 100, 1000];
 test.describe('"Add Element" button', () => {
   test('adds new "Delete" button', async ({ page }) => {
     await clickAddElementButton(page);
-    await checkVisibilityOfDeleteButton(page);
+    await checkCountOfDeleteButtons(page);
   });
 
   test('adds next "Delete" button after previous one', async ({ page }) => {
@@ -19,89 +19,116 @@ test.describe('"Add Element" button', () => {
 
   for (const clickCount of CLICK_COUNT) {
     test(`adds ${clickCount} "Delete" buttons after clicking "Add Element" ${clickCount} times`, async ({ page }) => {
-      // Given
-      const addElementButtonLocator = page.getByRole("button", { name: "Add Element" });
-
-      // When
-      await addElementButtonLocator.click({ clickCount: clickCount });
-
-      // Then
-      const deleteButtonsLocator = page.getByRole("button", { name: "Delete" });
-      await expect(deleteButtonsLocator).toHaveCount(clickCount);
+      await clickAddElementButton(page, clickCount);
+      await checkCountOfDeleteButtons(page, clickCount);
     });
   }
 });
 
-test.describe("Remove Element", () => {
+test.describe('"Delete" button', () => {
   test('removes clicked "Delete" button', async ({ page }) => {
-    // Given
-    const addElementButtonLocator = page.getByRole("button", { name: "Add Element" });
-    await addElementButtonLocator.click();
+    await clickAddElementButton(page);
+    await checkCountOfDeleteButtons(page, 1);
 
-    const deleteButtonLocator = page.getByRole("button", { name: "Delete" });
-    await expect(deleteButtonLocator).toBeVisible();
-
-    // When
-    await deleteButtonLocator.click();
-    
-    // Then
-    await expect(deleteButtonLocator).not.toBeVisible();
+    await clickDeleteButton(page);
+    await checkCountOfDeleteButtons(page, 0);
   });
 
   for (const clickCount of CLICK_COUNT) {
     test(`does not remove other ${clickCount - 1} "Delete" buttons`, async ({ page }) => {
-      // Given
-      const addElementButtonLocator = page.getByRole("button", { name: "Add Element" });
-      await addElementButtonLocator.click({ clickCount: clickCount });
-    
-      const deleteButtonLocator = page.getByRole("button", { name: "Delete" });
-  
-      // When
-      await deleteButtonLocator.first().click();
-      
-      // Then
-      await expect(deleteButtonLocator).toHaveCount(clickCount - 1);
+      await clickAddElementButton(page, clickCount);
+      await checkCountOfDeleteButtons(page, clickCount);
+
+      await clickDeleteButton(page, 1);
+      await checkCountOfDeleteButtons(page, clickCount - 1);
     });
   }
 
   for (const clickCount of CLICK_COUNT) {
     test(`removes all ${clickCount} clicked "Delete" buttons`, async ({ page }) => {
-      // Given
-      const addElementButtonLocator = page.getByRole("button", { name: "Add Element" });
-      await addElementButtonLocator.click({ clickCount: clickCount });
-    
-      const deleteButtonLocator = page.getByRole("button", { name: "Delete" });
-  
-      // When
-      await deleteButtonLocator.first().click({ clickCount: clickCount });
-      
-      // Then
-      await expect(deleteButtonLocator).toHaveCount(0);
+      await clickAddElementButton(page, clickCount);
+      await checkCountOfDeleteButtons(page, clickCount);
+
+      await clickDeleteButton(page, clickCount);
+      await checkCountOfDeleteButtons(page, 0);
     });
   }
+
+  test.skip('stays visible after refreshing page', async ({ page }) => {
+    await clickAddElementButton(page);
+    await checkCountOfDeleteButtons(page, 1);
+
+    await reloadPage(page);
+    await checkCountOfDeleteButtons(page, 1);
+  });
+
+  test.skip('stays visible after going back and forward', async ({ page }) => {
+    await clickAddElementButton(page);
+    await checkCountOfDeleteButtons(page, 1);
+
+    await goToPreviousPage(page);
+    await goToNextPage(page);
+    await checkCountOfDeleteButtons(page, 1);
+  });
 });
 
-async function checkVisibilityOfDeleteButton(page: Page) {
-  await test.step(`"Delete" button is visible`, async () => {
-    await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
+async function reloadPage(page: Page) {
+  await test.step(`Reload page.`, async () => {
+    await page.reload();
   });
 }
 
-async function checkCountOfDeleteButtons(page: Page, count: number) {
-  await test.step(`There are ${count} visible "Delete" buttons`, async () => {
-    const deleteButtonsLocator = page.getByRole("button", { name: "Delete" });
-    await expect(deleteButtonsLocator).toHaveCount(count);
+async function goToPreviousPage(page: Page) {
+  await test.step(`Go to previous page.`, async () => {
+    await page.goBack();
   });
+}
+
+async function goToNextPage(page: Page) {
+  await test.step(`Go to next page.`, async () => {
+    await page.goForward();
+  });
+}
+
+async function checkCountOfDeleteButtons(page: Page, count: number = 1) {
+  if (count === 0) {
+    await test.step(`"Delete" button is not visible.`, async () => {
+      const deleteButtonsLocator = page.getByRole("button", { name: "Delete" });
+      await expect(deleteButtonsLocator).toHaveCount(count);
+    });
+  } else if (count === 1) {
+    await test.step(`"Delete" button is visible.`, async () => {
+      const deleteButtonsLocator = page.getByRole("button", { name: "Delete" });
+      await expect(deleteButtonsLocator).toHaveCount(count);
+    });
+  } else {
+    await test.step(`There are ${count} visible "Delete" buttons.`, async () => {
+      const deleteButtonsLocator = page.getByRole("button", { name: "Delete" });
+      await expect(deleteButtonsLocator).toHaveCount(count);
+    });
+  }
 }
 
 async function clickAddElementButton(page: Page, clickCount: number = 1) {
   if (clickCount === 1) {
-    await test.step(`Click "Add Element" button`, async () => {
+    await test.step(`Click "Add Element" button.`, async () => {
       await page.getByRole("button", { name: "Add Element" }).click();
     });
   } else {
-    await test.step(`Click "Add Element" button ${clickCount} times`, async () => {
+    await test.step(`Click "Add Element" button ${clickCount} times.`, async () => {
       await page.getByRole("button", { name: "Add Element" }).click({ clickCount: clickCount });
+    });
+  }
+}
+
+async function clickDeleteButton(page: Page, clickCount: number = 1) {
+  if (clickCount === 1) {
+    await test.step(`Click "Delete" button.`, async () => {
+      await page.getByRole("button", { name: "Delete" }).first().click();
+    });
+  } else {
+    await test.step(`Click "Delete" button ${clickCount} times.`, async () => {
+      await page.getByRole("button", { name: "Delete" }).first().click({ clickCount: clickCount });
     });
   }
 }
